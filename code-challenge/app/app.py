ccad1c66fe4c55e3abae6ecbc153d407f9949a85
WinterJackson <winterjacksonwj@gmail.com>
2023-09-26 21:23:59 +0300

    GET_Heroes/id

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
-from models import db, Hero
+from models import db, Hero, Power
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -30,6 +30,32 @@ def get_heroes():
         for hero in heroes
     ]
     return jsonify(heroes_data)
+
+@app.route('/heroes/<int:id>', methods=['GET'])
+def get_hero_by_id(id):
+    hero = Hero.query.get(id)
+
+    if hero is None:
+        return make_response(jsonify({"error": "Hero not found"}), 404)
+
+    # Get powers associated with the hero
+    powers = []
+    for hero_power in hero.powers:
+        power = Power.query.get(hero_power.power_id)
+        powers.append({
+            "id": power.id,
+            "name": power.name,
+            "description": power.description
+        })
+
+    hero_data = {
+        "id": hero.id,
+        "name": hero.name,
+        "super_name": hero.super_name,
+        "powers": powers
+    }
+
+    return jsonify(hero_data)
 
 
 if __name__ == '__main__':
